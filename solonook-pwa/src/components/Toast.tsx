b768141d304d875fd42288f0e9929051e9db87f8
videoeditranah-ph <videoedit.ranah@gmail.com>
2026-02-19 06:13:56 +0800

    Delete solonook-pwa directory

--- solonook-pwa/src/components/Toast.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function Toast({ message }: { message: string | null }) {
-  if (!message) return null;
-  return <div className="toast">{message}</div>;
-}
